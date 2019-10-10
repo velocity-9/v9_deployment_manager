de86c1451d1f20cb33cbf416ea2f5726c0d24390
Henry Jaensch <hjaensch7@gmail.com>
2019-10-10 17:14:46 -0400

    Basic go server configured to receive github webhook events (#1)
    * Listens for push events and prints json when it gets one
    
    * change error handling, onle print payload if no error received from hook.parse
    
    * Updated error handling

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/go-playground/webhooks.v5/github"
+	"net/http"
+)
+
+const (
+	path = "/payload"
+)
+
+func main() {
+	hook, err := github.New(github.Options.Secret("<GITHUBSECRET>"))
+	if err != nil {
+		fmt.Println("github.New Error", err)
+	}
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.PushEvent)
+		if err != nil {
+			fmt.Println("github payload parse failed", err)
+			return
+		}
+		fmt.Println("Received Payload:")
+		push := payload.(github.PushPayload)
+		fmt.Println("PUSH PAYLOAD:")
+		fmt.Println("%+v", push)
+	})
+
+	fmt.Println("Starting Server...")
+	http.ListenAndServe(":3069", nil)
+}
