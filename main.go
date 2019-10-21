0719940f9de6ccf59e3314e11ffbabe2869932ce
hjaensch <hjaensch7@gmail.com>
2019-10-21 19:01:39 -0400

    Server downloads repo contents upon receiving push event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-getter"
 	"gopkg.in/go-playground/webhooks.v5/github"
 	"net/http"
 )
@@ -11,21 +12,22 @@ const (
 )
 
 func main() {
-	hook, err := github.New(github.Options.Secret("<GITHUBSECRET>"))
+	hook, err := github.New(github.Options.Secret("thespeedeq"))
 	if err != nil {
-		fmt.Println("github.New Error", err)
+		fmt.Println("github.New Error:", err)
 	}
 
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		payload, err := hook.Parse(r, github.PushEvent)
 		if err != nil {
-			fmt.Println("github payload parse failed", err)
+			fmt.Println("github payload parse error:", err)
 			return
 		}
 		fmt.Println("Received Payload:")
 		push := payload.(github.PushPayload)
-		fmt.Println("PUSH PAYLOAD:")
-		fmt.Println(push)
+		downloadURL := getHTTPDownloadURL(push)
+		fmt.Println("Download URL: ", downloadURL)
+		downloadRepo(downloadURL)
 	})
 
 	fmt.Println("Starting Server...")
@@ -33,4 +35,17 @@ func main() {
 	if err != nil {
 		fmt.Println("http.http.ListenAndServe Error", err)
 	}
+}
+
+func getHTTPDownloadURL(p github.PushPayload) string {
+	return "git::" + p.Repository.URL
+}
+
+func downloadRepo(downloadURL string) {
+	err := getter.Get("./repo", downloadURL)
+	if err != nil {
+		fmt.Println("Error downloading repo:", err)
+		return
+	}
+	fmt.Println("Repo downloaded from:", downloadURL)
 }
